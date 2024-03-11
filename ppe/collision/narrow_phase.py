b074f201aa929e7acfea6325974551781e857c90
Moritz Hesche <mo.hesche@gmail.com>
2024-03-11 22:43:29 +0100

    Implement collision detection for ball-polygon and polygon-polygon collisions

--- ppe/collision/narrow_phase.py
+++ ppe/collision/narrow_phase.py
@@ -1,5 +1,6 @@
 from typing import List, Tuple
 import abc
+import math
 
 from ppe.bodies import Body, Ball, ConvexPolygon
 from ppe.collision.collision_data import Collision
@@ -14,63 +15,105 @@ class NarrowPhaseBase(abc.ABC):
 
 
 class SAT(NarrowPhaseBase):
-    def ball_ball_collision(self, ball1: Ball, ball2: Ball) -> List[Collision]:
-        delta = ball2.com - ball1.com
+    def ball_ball_collision(self, ball1: Body, ball2: Body) -> List[Collision]:
+        assert isinstance(ball1.shape, Ball) and isinstance(ball2.shape, Ball)
+
+        delta = ball2.shape.com - ball1.shape.com  # from ball1 to ball2
         dist = delta.magnitude()
 
-        if dist < ball1.radius + ball2.radius:
+        if dist < ball1.shape.radius + ball2.shape.radius:
             normal = delta.normalize()
-            return [
-                Collision(
-                    bodyA=ball1,
-                    bodyB=ball2,
-                    normal=normal,
-                    depth=ball1.radius + ball2.radius - dist,
-                    contact_point_1=ball1.com + normal * ball1.radius,
-                )
-            ]
+            penetration_depth = ball1.shape.radius + ball2.shape.radius - dist
+            collision = Collision(
+                bodyA=ball1,
+                bodyB=ball2,
+                normal=normal,
+                depth=penetration_depth,
+                penetrating_point=ball1.shape.com
+                + (ball1.shape.radius - penetration_depth) * normal,
+            )
+            return [collision]
 
         return []
 
-    def ball_polygon_collision(
-        self, ball: Ball, polygon: ConvexPolygon
-    ) -> List[Collision]:
+    def ball_polygon_collision(self, ball: Body, polygon: Body) -> List[Collision]:
+        assert isinstance(ball.shape, Ball) and isinstance(polygon.shape, ConvexPolygon)
+
         # https://www.youtube.com/watch?v=vWs33LVrs74
-        # ball_axis = None
-        # ball_axis_magnitude = float("inf")
 
-        # for vertex in polygon.vertices:
-        #     axis = vertex - ball.com  # from ball to vertex
-        #     if axis.magnitude() < ball_axis_magnitude:
-        #         ball_axis = axis.normalize()
-        #         ball_axis_magnitude = axis.magnitude()
+        min_penetration_depth = float("inf")
+        collision = None
 
-        # axes = chain(polygon.get_normals(), [ball_axis])
+        # first we analyze the normals of the polygon as potential separating axes
+        for axis, normal_point_1 in zip(polygon.shape.normals, polygon.shape.vertices):
+            # projection length of the points associated with the normal on the normal
+            projected_normal_points = axis.dot(normal_point_1)
+            projected_ball_center = axis.dot(ball.shape.com)
+            penetration_depth = (
+                projected_normal_points - projected_ball_center + ball.shape.radius
+            )
 
-        # collision = _sat(ball, polygon, axes)
-        # # !!! contact points are not calculated in _sat
-        # # the collision returned by _sat has a normal which always points from obj1 to obj2
-        # # obj1 is the ball and obj2 is the polygon
-        # if collision is not None:
-        #     collision.contact_point_1 = ball.pos + collision.normal * (
-        #         ball.radius - collision.depth
-        #     )
+            # there is a gap when the closest point of the ball is in front of the normal
+            if penetration_depth < 0:
+                return []
 
-        # return collision
-        raise NotImplementedError
+            # otherwise we might have a collision
+            if penetration_depth < min_penetration_depth:
+                collision = Collision(
+                    bodyA=polygon,
+                    bodyB=ball,
+                    normal=axis,
+                    depth=penetration_depth,
+                    penetrating_point=ball.shape.com - axis * ball.shape.radius,
+                )
+                min_penetration_depth = penetration_depth
+
+        # we also need to check the axis from the ball to the closest vertex of the polygon
+        # this can be seen as equivalent to the normal of the edge of the polygon
+        # we use the sqaured magnitude to avoid the square root computation
+        ball_axis = None
+        ball_axis_squared_magnitude = float("inf")
+        for vertex in polygon.shape.vertices:
+            axis = vertex - ball.shape.com  # from ball to vertex
+            new_squared_ball_axis_magnitude = axis.squared_magnitude()
+            if new_squared_ball_axis_magnitude < ball_axis_squared_magnitude:
+                ball_axis = axis  # / math.sqrt(new_squared_ball_axis_magnitude)
+                ball_axis_squared_magnitude = new_squared_ball_axis_magnitude
+
+        ball_axis_magnitude = math.sqrt(ball_axis_squared_magnitude)
+        # there is a collision if the ball is inside the polygon
+        penetration_depth = ball.shape.radius - ball_axis_magnitude
+        if penetration_depth < 0:
+            return []
+
+        if penetration_depth < min_penetration_depth:
+            collision = Collision(
+                bodyA=ball,
+                bodyB=polygon,
+                normal=ball_axis / ball_axis_magnitude,
+                depth=penetration_depth,
+                penetrating_point=ball.shape.com - ball_axis,
+            )
+
+        return [collision]
 
     def polygon_polygon_collision(
-        self, polygon1: ConvexPolygon, polygon2: ConvexPolygon
+        self, polygon1: Body, polygon2: Body
     ) -> List[Collision]:
-        collision_normal = None
-        min_penetration = float("inf")
+        assert isinstance(polygon1.shape, ConvexPolygon) and isinstance(
+            polygon2.shape, ConvexPolygon
+        )
 
-        for axis in polygon1.normals:
-            min1, max1 = polygon1.projected_extends(axis)
-            min2, max2 = polygon2.projected_extends(axis)
+        # collision_normal = None
+        # min_penetration = float("inf")
 
-            if max1 < min2 or max2 < min1:
-                return []
+        # for axis in polygon1.normals:
+        #     min1, max1 = polygon1.projected_extends(axis)
+        #     min2, max2 = polygon2.projected_extends(axis)
+
+        #     if max1 < min2 or max2 < min1:
+        #         return []
+        raise NotImplementedError
 
     def __call__(
         self, collision_candidates: List[Tuple[Body, Body]]
